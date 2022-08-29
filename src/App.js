2d28c5dd252dcfad5c3bc1e2d73392e1e1d8500f
Strong10mede <kmayur809@gmail.com>
2022-08-30 01:42:32 +0530

    header inclusion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="app">
       <h1>Building youtube clone 🚀</h1>
+      <Header />
     </div>
   );
 }
