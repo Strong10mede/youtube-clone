90104e095c8f401187a740e608dc206ab9fe558e
Strong10mede <kmayur809@gmail.com>
2022-08-30 23:34:22 +0530

    changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,18 +9,24 @@ import { Avatar } from "@mui/material";
 function Header() {
   return (
     <div className="header">
-      <MenuIcon />
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/YouTube_Logo_%282013-2017%29.svg/2560px-YouTube_Logo_%282013-2017%29.svg.png"
-        alt=""
-        className="header__logo"
-      />
-      <input type="text" />
-      <SearchIcon />
-      <VideoCallIcon />
-      <AppsIcon />
-      <NotificationsIcon />
-      <Avatar alt="Mede" src="ss" />
+      <div className="header__left">
+        <MenuIcon />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/YouTube_Logo_%282013-2017%29.svg/2560px-YouTube_Logo_%282013-2017%29.svg.png"
+          alt=""
+          className="header__logo"
+        />
+      </div>
+      <div className="header__input">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+      <div className="header__icons">
+        <VideoCallIcon />
+        <AppsIcon />
+        <NotificationsIcon />
+        <Avatar alt="Mede" src="ss" />
+      </div>
     </div>
   );
 }
