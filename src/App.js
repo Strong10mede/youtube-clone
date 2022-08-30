115b887a0b537c463a6e3ad0a42e65f720ee2742
Strong10mede <kmayur809@gmail.com>
2022-08-30 23:51:06 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Sidebar from "./Components/Sidebar";
+import RecommendVideos from "./Components/RecommendVideos";
 import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <div className="app__page">
+        <Sidebar />
+        <RecommendVideos />
+      </div>
     </div>
   );
 }
