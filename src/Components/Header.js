93e75574c2e63d3072e01d8b40c0862e02492b96
Strong10mede <kmayur809@gmail.com>
2022-08-31 22:28:19 +0530

    header.js setup

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
@@ -6,26 +6,42 @@ import VideoCallIcon from "@mui/icons-material/VideoCall";
 import AppsIcon from "@mui/icons-material/Apps";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { Avatar } from "@mui/material";
+import { Link } from "react-router-dom";
 function Header() {
+  const [inputSearch, setInputSearch] = useState("");
   return (
     <div className="header">
       <div className="header__left">
         <MenuIcon />
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/YouTube_Logo_%282013-2017%29.svg/2560px-YouTube_Logo_%282013-2017%29.svg.png"
-          alt=""
-          className="header__logo"
-        />
+        <Link>
+          <img
+            className="header__logo"
+            src="https://upload.wikimedia.org/wikipedia/id/thumb/b/b5/Youtube_Logo_2005.png/250px-Youtube_Logo_2005.png"
+            alt=""
+          />
+        </Link>
       </div>
+
       <div className="header__input">
-        <input type="text" placeholder="Search" />
-        <SearchIcon className="header__inputButton" />
+        <input
+          onChange={(e) => setInputSearch(e.target.value)}
+          value={inputSearch}
+          placeholder="Search"
+          type="text"
+        />
+        <Link to={`/search/${inputSearch}`}>
+          <SearchIcon className="header__inputButton" />
+        </Link>
       </div>
+
       <div className="header__icons">
         <VideoCallIcon className="header__icon" />
         <AppsIcon className="header__icon" />
         <NotificationsIcon className="header__icon" />
-        <Avatar alt="Mede" src="ss" />
+        <Avatar
+          alt="Tomnomnom"
+          src="https://avatars0.githubusercontent.com/u/50357969?s=460&u=95f9a7d2c9083d9beb17945c4e358e0aa3421ce7&v=4"
+        />
       </div>
     </div>
   );
