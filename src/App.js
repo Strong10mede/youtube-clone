e93a13919efab282f27fc93ea151d6fd56566d4a
Strong10mede <kmayur809@gmail.com>
2022-08-31 22:19:38 +0530

    csssetting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
+import React from "react";
 import "./App.css";
-import Sidebar from "./Components/Sidebar";
-import RecommendVideos from "./Components/RecommendVideos";
-import Header from "./Components/Header";
+import Header from "./Header/Header";
+import Sidebar from "./Sidebar/Sidebar";
+import SearchPage from "./Header/SearchPage";
+import RecommendedVideos from "./RecommendedVideos/RecommendedVideos";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="app__page">
-        <Sidebar />
-        <RecommendVideos />
-      </div>
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/search/:searchTerm">
+            <div className="app__page">
+              <Sidebar />
+              <SearchPage />
+            </div>
+          </Route>
+          <Route path="/">
+            <div className="app__page">
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
