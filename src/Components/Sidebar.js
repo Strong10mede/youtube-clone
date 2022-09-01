50cef23f23180cee379b3ef8b84d48b7c003d959
Strong10mede <kmayur809@gmail.com>
2022-09-01 11:41:17 +0530

    sidebar.js setup

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,15 +1,31 @@
 import React from "react";
-import "./Sidebar.css";
-import SidebarRow from "./SidebarRow";
 import HomeIcon from "@mui/icons-material/Home";
 import WhatshotIcon from "@mui/icons-material/Whatshot";
 import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+import HistoryIcon from "@mui/icons-material/History";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import ThumbUpAltOutLinedIcon from "@mui/icons-material/ThumbUpAlt";
+import ExpandMoreOutLinedIcon from "@mui/icons-material/ExpandMore";
+import SidebarRow from "./SidebarRow";
+
+import "./Sidebar.css";
+
 function Sidebar() {
   return (
     <div className="sidebar">
-      <SidebarRow icon={HomeIcon} title="Home" />
-      <SidebarRow icon={WhatshotIcon} title="Trending" />
-      <SidebarRow icon={SubscriptionsIcon} title="Subscription" />
+      <SidebarRow selected Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={WhatshotIcon} title="Trending" />
+      <SidebarRow Icon={SubscriptionsIcon} title="Subscription" />
+      <hr />
+      <SidebarRow Icon={VideoLibraryIcon} title="Library" />
+      <SidebarRow Icon={HistoryIcon} title="History" />
+      <SidebarRow Icon={OndemandVideoIcon} title="Your Videos" />
+      <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
+      <SidebarRow Icon={ThumbUpAltOutLinedIcon} title="Liked video" />
+      <SidebarRow Icon={ExpandMoreOutLinedIcon} title="Show more" />
+      <hr />
     </div>
   );
 }
